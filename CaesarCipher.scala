b8ce60db8e327fd2faa1e0e82bf6cbf54a237463
shashira <shashiradw7@gmail.com>
2020-07-16 18:38:11 +0000

    Caesar Cipher. scala file added

---
+++ CaesarCipher.scala
@@ -0,0 +1,33 @@
+import scala.io.StdIn._
+
+object CaesarCipher {
+    
+    def main(args: Array[String]){
+        
+     val alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+     //READ WORD
+     print("Enter the Word >")
+     var word=readLine()
+
+     //ASK E OR D
+     print("Enter E-Encrypt or D-Decrypy >")
+     val flag:Char=readChar().toUpper
+     val shift:Int=3
+
+     val E =(c:Char,shift:Int,a:String)=> a((a.indexOf(c.toUpper)+shift)%26)
+
+     val D =(c:Char,shift:Int,a:String)=> a((a.indexOf(c.toUpper)-shift)%26)
+
+
+      val cipher=(algo:(Char,Int,String)=>Char,word:String,a:String,shift:Int)=> word.map(algo(_,shift,a));
+    
+      //CALL
+      
+      val c= if(flag=='E') cipher(E,word,alphabet,shift) else cipher(D,word,alphabet,shift) 
+      println(c);
+
+
+    }
+
+}
